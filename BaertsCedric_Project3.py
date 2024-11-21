9abe90844a685a3b4f14e2105054d9393cfe0900
Cbaerts <cbaerts@uwo.ca>
2024-11-21 15:46:40 -0500

    Update BaertsCedric_Project3.py
    Added Constants

--- BaertsCedric_Project3.py
+++ BaertsCedric_Project3.py
@@ -1,1 +1,13 @@
 import numpy as np
+import scipy
+import scipy.integrate as scint
+import matplotlib.pyplot as plt
+
+# Constants
+Mue = 2
+R0 = 7.72*10**8 / Mue
+M0 = 5.67*10**33 / Mue**2
+Mch = 5.836 / Mue**2
+Rho0 = 9.74*10**5 * Mue
+
+def solver():
