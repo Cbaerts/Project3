aa8909ab732ad102c7cb148b35a0ab2307c70b7f
Cbaerts <cbaerts@uwo.ca>
2024-11-21 16:51:21 -0500

    Update BaertsCedric_Project3.py
    added the range for RhoC and units

--- BaertsCedric_Project3.py
+++ BaertsCedric_Project3.py
@@ -5,9 +5,20 @@ import matplotlib.pyplot as plt
 
 # Constants
 Mue = 2
-R0 = 7.72*10**8 / Mue
-M0 = 5.67*10**33 / Mue**2
-Mch = 5.836 / Mue**2
-Rho0 = 9.74*10**5 * Mue
+R0 = 7.72*10**8 / Mue #cm
+M0 = 5.67*10**33 / Mue**2 #g
+Mch = 5.836 / Mue**2 #g
+m = 0
+Rho0 = 9.74*10**5 * Mue #g/cm**3
+MSun = 1.989*10**33 #g
+RSun = 6.963*10**10 #cm
 
-def solver():
+RhoC = np.logspace(-1, 6.4, 10)
+print(RhoC)
+
+def dRhodR(): 
+    return 'Hello World'
+
+# BoldR = 
+# BoldM = 
+# BoldRho =
